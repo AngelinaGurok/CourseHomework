a4ab498b4f5ea952061737c4ec96d8fd1ab5ead7
Angelina <a.hurok@gmail.com>
2024-10-29 23:53:44 +0300

    added function to create hashmap

--- src/collections/maps/ClassRegister.java
+++ src/collections/maps/ClassRegister.java
@@ -1,7 +1,10 @@
 package collections.maps;
 
+import java.lang.foreign.StructLayout;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 /**
 
  * По заданному ассоциативному массиву "студент"-"оценка за экзамен" построить
@@ -11,19 +14,27 @@ import java.util.Map;
  *   buildGrades(mapOf("Марат" to 3, "Семён" to 5, "Михаил" to 5))
  *     -> mapOf(5 to listOf("Семён", "Михаил"), 3 to listOf("Марат"))
  */
-abstract public class ClassRegister {
-   String name;
-   Integer mark;
+public class ClassRegister {
+   HashMap<String, Integer> register;
 
    ClassRegister(){
-      name = "Undefined";
-      mark = 0;
+       register = new HashMap<String, Integer>();
    }
 
-   ClassRegister(String name, Integer mark){
-      this.name = name;
-      this.mark = mark;
+   public void fulfillRegister(){
+       String inputCheck = "y";
+       Scanner in = new Scanner(System.in);
+       String name;
+       Integer mark;
+       while(inputCheck == "y" || inputCheck == "Y"){
+           System.out.print("\nInput name:");
+           name = in.nextLine();
+           System.out.print("\nInput mark: ");
+           mark = in.nextInt();
+           this.register.put(name, mark);
+           System.out.print("Do you want to add one more record? y/n");
+           inputCheck = in.nextLine();
+       }
    }
 
-   
 }
