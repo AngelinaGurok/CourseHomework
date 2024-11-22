06f2794caed52ff38df49cef50d96fbf2a718dbb
Angelina <a.hurok@gmail.com>
2024-11-22 23:27:25 +0300

    added class Friends

---
+++ src/collections/friends/Friends.java
@@ -0,0 +1,47 @@
+package collections.friends;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Сложная (5 баллов)
+ *
+ * Для заданного ассоциативного массива знакомых через одно рукопожатие friends
+ * необходимо построить его максимальное расширение по рукопожатиям, то есть,
+ * для каждого человека найти всех людей, с которыми он знаком через любое
+ * количество рукопожатий.
+ *
+ * Считать, что все имена людей являются уникальными, а также что рукопожатия
+ * являются направленными, то есть, если Марат знает Свету, то это не означает,
+ * что Света знает Марата.
+ *
+ * Оставлять пустой список знакомых для людей, которые их не имеют (см. EvilGnome ниже),
+ * в том числе для случая, когда данного человека нет в ключах, но он есть в значениях
+ * (см. GoodGnome ниже).
+ *
+ * Например:
+ *   propagateHandshakes(
+ *     mapOf(
+ *       "Marat" to setOf("Mikhail", "Sveta"),
+ *       "Sveta" to setOf("Marat"),
+ *       "Mikhail" to setOf("Sveta"),
+ *       "Friend" to setOf("GoodGnome"),
+ *       "EvilGnome" to setOf()
+ *     )
+ *   ) -> mapOf(
+ *          "Marat" to setOf("Mikhail", "Sveta"),
+ *          "Sveta" to setOf("Marat", "Mikhail"),
+ *          "Mikhail" to setOf("Sveta", "Marat"),
+ *          "Friend" to setOf("GoodGnome"),
+ *          "EvilGnome" to setOf(),
+ *          "GoodGnome" to setOf()
+ *        )
+ */
+public class Friends {
+    Map<String, String> friends;
+
+    public Friends(){
+        friends = new HashMap<>();
+    }
+}
