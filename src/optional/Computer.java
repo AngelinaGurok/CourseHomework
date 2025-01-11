f72636873d549034a47e2b6e111350cbce5ef9ea
Angelina <a.hurok@gmail.com>
2025-01-11 17:33:44 +0300

    added demonstration of Optional class work

--- src/optional/Computer.java
+++ src/optional/Computer.java
@@ -1,17 +1,18 @@
 package optional;
 
+import java.util.Optional;
+
 public class Computer {
-    Soundcard s;
-
-    Computer(){
-        s = new Soundcard();
-    }
-
-    Computer(String version){
-        s = new Soundcard(version);
-    }
-
-    public Soundcard getSoundcard() {
+    private Optional<Soundcard> s;
+    public Optional<Soundcard> getSoundcard() {
         return s;
+    }
+
+    Computer(Soundcard sc){
+        this.s = Optional.of(sc);
+    }
+
+    public void setSoundcard(Optional<Soundcard> s) {
+        this.s = s;
     }
 }
